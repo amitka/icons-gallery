e755156fb608866fdc4b52554207466c60bbb93a
Amit Kaplisnky <you@example.com>
2020-07-06 10:47:22 +0300

    Updates

---
+++ precache-manifest.a548d9acc54677345eb98c55a721a90e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b38718d980a1feb9e72c556f86f8bd1d",
+    "url": "/icons-gallery/index.html"
+  },
+  {
+    "revision": "027ad13a06c540c15deb",
+    "url": "/icons-gallery/static/css/main.63996b05.chunk.css"
+  },
+  {
+    "revision": "240bd5b247c1b54bbccd",
+    "url": "/icons-gallery/static/js/2.1b181fa5.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/icons-gallery/static/js/2.1b181fa5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "027ad13a06c540c15deb",
+    "url": "/icons-gallery/static/js/main.cfca0097.chunk.js"
+  },
+  {
+    "revision": "622c2e0c980df91c9b0e",
+    "url": "/icons-gallery/static/js/runtime-main.aaf517bc.js"
+  }
+]);
